3d02b358762004cbb091c06db855ecd7ad92b734
s-andrzejewski <s.andrzejewski.7@gmail.com>
2023-11-14 09:10:54 +0100

    Initial module config

---
+++ translations/pl.php
@@ -0,0 +1,13 @@
+<?php
+
+$_MODULES = array(
+    'imagetagshowcase' => array(
+        'A module which allows you to tag products on a photo.' => 'Moduł, który umożliwia tagowanie produktów na zdjęciu.',
+        'Displays custom banner on homepage.' => 'Wyświetla niestandardowy baner na stronie głównej.',
+        'Are you sure you want to uninstall this module?' => 'Czy na pewno chcesz odinstalować ten moduł?',
+        'Settings' => 'Ustawienia',
+        'Upload image' => 'Prześlij obraz',
+        'Save' => 'Zapisz',
+        'Nothing here.' => 'Nic tu nie ma.',
+    )
+);
